2ae7c47d95bac04b9746d8b052e09d09e93b2a09
Pedro Brito <tablet.pfb@gmail.com>
2025-10-10 09:11:35 -0300

    Requesting basic user input

--- radix.c
+++ radix.c
@@ -5,11 +5,21 @@
 
 int *Allocate_int(int N); // Alocates an integer vector
 
+// ------------ MAIN -------------//
 int main() {
 
   int N; // number of integers
   scanf("%d", &N);
+
+  int *numbers = Allocate_int(N); // the numbers provided by the user
+  for (int i = 0; i < N; i++) scanf("%d", &numbers[i]);
+
+  free(numbers);
+
+  return 0;
 }
+
+// ------- IMPLEMENTATIONS -------------//
 
 int *Allocate_int(int N) {
   return (int *)malloc(sizeof(int) * N);
