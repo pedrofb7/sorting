a554aa1a235980d589b0eeb19277c016fbd897c3
MatheusSartin <msartinbrito@gmail.com>
2025-10-10 10:15:27 -0300

    Couting Sort

--- radix.c
+++ radix.c
@@ -5,6 +5,32 @@
 
 int *Allocate_int(int N); // Alocates an integer vector
 
+
+
+// ------------Counting Sort-------------//
+void countingSort(int arr[], int n, int exp) {
+    int output[n];
+    int count[10] = {0};
+    for (int i = 0; i < n; i++) {
+        int digit = (arr[i] / exp) % 10;
+        count[digit]++;
+    }
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        int digit = (arr[i] / exp) % 10;
+        output[count[digit] - 1] = arr[i];
+        count[digit]--;
+    }
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+
+
+
 // ------------ MAIN -------------//
 int main() {
 
@@ -15,6 +41,8 @@ int main() {
   for (int i = 0; i < N; i++) scanf("%d", &numbers[i]);
 
   free(numbers);
+
+  
 
   return 0;
 }
