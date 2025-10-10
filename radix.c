fcb6c915d256faa31750c67dcfc1e666fb9ab03e
Pedro Brito <tablet.pfb@gmail.com>
2025-10-10 10:33:26 -0300

    Improving readability

--- radix.c
+++ radix.c
@@ -4,32 +4,7 @@
 // ---------- FUNCTIONS ---------- //
 
 int *Allocate_int(int N); // Alocates an integer vector
-
-
-
-// ------------Counting Sort-------------//
-void countingSort(int arr[], int n, int exp) {
-    int output[n];
-    int count[10] = {0};
-    for (int i = 0; i < n; i++) {
-        int digit = (arr[i] / exp) % 10;
-        count[digit]++;
-    }
-    for (int i = 1; i < 10; i++) {
-        count[i] += count[i - 1];
-    }
-    for (int i = n - 1; i >= 0; i--) {
-        int digit = (arr[i] / exp) % 10;
-        output[count[digit] - 1] = arr[i];
-        count[digit]--;
-    }
-    for (int i = 0; i < n; i++) {
-        arr[i] = output[i];
-    }
-}
-
-
-
+void countingSort(int arr[], int n, int exp);
 
 // ------------ MAIN -------------//
 int main() {
@@ -42,13 +17,33 @@ int main() {
 
   free(numbers);
 
-  
-
   return 0;
 }
 
 // ------- IMPLEMENTATIONS -------------//
 
+// ------------ Alocation --------------//
 int *Allocate_int(int N) {
   return (int *)malloc(sizeof(int) * N);
 };
+
+// ------------Counting Sort-------------//
+void countingSort(int arr[], int n, int exp) {
+  int output[n];
+  int count[10] = {0};
+  for (int i = 0; i < n; i++) {
+    int digit = (arr[i] / exp) % 10;
+    count[digit]++;
+  }
+  for (int i = 1; i < 10; i++) {
+    count[i] += count[i - 1];
+  }
+  for (int i = n - 1; i >= 0; i--) {
+    int digit = (arr[i] / exp) % 10;
+    output[count[digit] - 1] = arr[i];
+    count[digit]--;
+  }
+  for (int i = 0; i < n; i++) {
+    arr[i] = output[i];
+  }
+}
